39f61dcac6411a14a803a32a419f0cf3f0ade0fa
Poomsupp <264318241008-st@rmutsb.ac.th>
2023-08-08 11:52:30 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import react from 'react'
+//import ArrayMap from './array-map';
+//import ColumnItem from './column-item';
+//import RowItem from './row-item';
+//import LayoutItem from './Layout-item';
+//import Question from './Question';
+//import Image from './image';
+//import ButtonDemo from './button-demo';
+//import ButtonStateDemo from './button-state-demo';
+import TextInputDemo from './text-input-demo';
+
+export default function App() {
+  return (
+      //[<Question/>,<Image/>,<LayoutItem/>]
+      //<ButtonDemo/>
+      //<ButtonStateDemo/>
+      <TextInputDemo/>
+  );
+}
